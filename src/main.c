528bfa78e46faa6db026c7fa8107192f05ab9efa
Gabriel Shiva Gomes Oliveira <gabrielshivagomes@gmail.com>
2025-07-28 23:51:14 -0300

    adiciona o contador de tempo

--- src/main.c
+++ src/main.c
@@ -25,34 +25,17 @@ static volatile uint32_t last_btn_a_press = 0;
 static volatile uint32_t last_btn_b_press = 0;
 static volatile uint32_t last_btn_sw_press = 0;
 
-// Variáveis de estado do sistema
-static volatile bool data_logger_state = false;
+// Flag que define se o cartão SD foi montado ou não
 static volatile bool is_mount_runned = false;
 
 // Informações do arquivo gerado
-static char file_name[20] = "adc_data.csv";
+static char file_name[20] = "adc_col_data.csv";
 
-static volatile uint counter = 0;
+// Definição de contadores que controlam estados temporários no sistema
+static volatile uint mount_counter = 0;
 static volatile uint file_counter = 0;
 
-typedef enum {
-    DISPLAY_MENU,
-    DISPLAY_ACTION_MESSAGE
-} display_state_t;
-
-static display_state_t display_state = DISPLAY_MENU;
-
-typedef struct {
-    char line1[22];
-    char line2[22];
-    char line3[22];
-    uint32_t duration_ms;
-    absolute_time_t start_time;
-    bool active;
-} action_message_t;
-
-static action_message_t current_message = {0};
-
+// Definição de estrutura e variável das páginas do display
 typedef enum {
     MENU_MAIN = 0,
     MENU_SAMPLING = 1,
@@ -61,20 +44,22 @@ typedef enum {
 
 static volatile menu_page_t menu_page = MENU_MAIN;
 
-absolute_time_t led_time = 0;
+// Definição de variável que armazena o tempo em que o led fica ligado durante a amostragem
+static absolute_time_t led_time = 0;
 
+// Definição dos estados de operação da coleta de dados
 typedef enum {
     SAMPLING_IDLE,
     SAMPLING_RUNNING,
     SAMPLING_STOPPING
 } sampling_state_t;
 
-volatile sampling_state_t sampling_state = SAMPLING_IDLE;
+static volatile sampling_state_t sampling_state = SAMPLING_IDLE;
 
 // Definição de variáveis para os valores lidos pelos sensores
-int16_t accel[3];
-int16_t gyro[3];
-int16_t temp;
+static int16_t accel[3];
+static int16_t gyro[3];
+static int16_t temp;
 
 typedef struct sensor_data {
     float accel_x;
@@ -85,29 +70,31 @@ typedef struct sensor_data {
     float gyro_z;
 } sensor_data_t;
 
-sensor_data_t sensor_data;
+static sensor_data_t sensor_data;
 
-ssd1306_t ssd;
-
+// Definição de variáveis para operação do display
+static ssd1306_t ssd;
 static bool color = true;
-char buffer[100];
-
-uint display_update_counter = 0;
-volatile bool needs_redraw = true;
+static char buffer[100];
+static volatile bool needs_redraw = true;
 
 // Definição do protótipo das funções que serão criadas
-void gpio_irq_handler(uint gpio, uint32_t events);
-void show_main_menu();
-void show_action_message(const char* l1, const char* l2, const char* l3, uint32_t duration);
-void show_sampling_menu();
-void get_sensor_data();
+static void gpio_irq_handler(uint gpio, uint32_t events);
+static void show_action_message(const char* l1, const char* l2, const char* l3, uint32_t duration);
+static void show_main_menu();
+static void show_sampling_menu();
+static void get_sensor_data();
+static void process_stdio(int cRxedChar);
+
+static absolute_time_t start_time;
 
 int main() {
     stdio_init_all();
 
-    // Iniciallização dos botões
+    // Inicialização dos botões
     btns_init();
 
+    // Habilita a interrupção para os botões A, B e SW
     gpio_set_irq_enabled_with_callback(BTN_B_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled(BTN_A_PIN, GPIO_IRQ_EDGE_RISE, true);
     gpio_set_irq_enabled(BTN_SW_PIN, GPIO_IRQ_EDGE_RISE, true);
@@ -116,7 +103,7 @@ int main() {
     leds_init();
 
     // Inicialização dos buzzers
-    buzzer_setup(BUZZER_RIGHT_PIN);
+    buzzer_setup(BUZZER_LEFT_PIN);
 
     // Inicialização do barramento I2C sensores AHT20 e BMP280
     i2c_setup(I2C0_SDA, I2C0_SCL);
@@ -154,32 +141,43 @@ int main() {
     ssd1306_fill(&ssd, !color);
     ssd1306_send_data(&ssd);
 
+    // Criação de instância para o arquiv
     FIL file;
     uint file_open_counter = 0;
 
     while (true) {
-        // Exibição do menu principal
-        if (display_state == DISPLAY_MENU) {
-            switch (menu_page) {
-                case MENU_MAIN:
-                    if (needs_redraw) {
-                        show_main_menu();
-                        needs_redraw = false;
-                    }
-                    break;
-
-                case MENU_SAMPLING:
-                    if (needs_redraw) {
-                        show_sampling_menu();
-                        needs_redraw = false;
-                    }
-                    break;
-
-                default:
-                    break;
-            }
+        // Realiza a leitura da entrada do terminal serial
+        int cRxedChar = getchar_timeout_us(0);
+        if (PICO_ERROR_TIMEOUT != cRxedChar) {
+            process_stdio(cRxedChar);
         }
 
+        // Exibe o conteúdo do arquivo se pressionar 'd'
+        if (cRxedChar == 'd') {
+            read_file(file_name);
+        }
+
+        // Exibição do menu principal
+        switch (menu_page) {
+            case MENU_MAIN:
+                if (needs_redraw) { // Verifica se é necessário atualizar o display
+                    show_main_menu();
+                    needs_redraw = false;
+                }
+                break;
+
+            case MENU_SAMPLING:
+                if (needs_redraw) { // Verifica se é necessário atualizar o display
+                    show_sampling_menu();
+                    needs_redraw = false;
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        // Controle do LED azul durante a coleta de dados
         if (absolute_time_diff_us(led_time, get_absolute_time()) / 1000 > 600 && sampling_state == SAMPLING_RUNNING) {
             led_time = get_absolute_time();
             leds_turnoff();
@@ -189,9 +187,7 @@ int main() {
         }
 
         // Realizar a montagem do cartão SD (pressionar BTN B)
-        if (is_mount_runned && counter == 0) {
-            printf("mount\n");
-
+        if (is_mount_runned && mount_counter == 0) {
             leds_turnoff();
             gpio_put(BLUE_LED_PIN, 1);
 
@@ -205,21 +201,21 @@ int main() {
                 show_action_message("Cartao SD", "Montado com", "Sucesso", 2500);
                 is_mount_runned = true;
                 needs_redraw = true;
-                counter = 1;
+                mount_counter = 1;
             } else {
                 leds_turnoff();
                 gpio_put(RED_LED_PIN, 1);
                 show_action_message("Falha ao", "Montar o", "Cartao SD", 2500);
                 is_mount_runned = false;
                 needs_redraw = true;
-                counter = 0;
+                mount_counter = 0;
             }
 
             leds_turnoff();
         }
 
         // Realizar a desmontagem do cartão SD (pressionar BTN B)
-        if (!is_mount_runned && counter == 1) {
+        if (!is_mount_runned && mount_counter == 1) {
             leds_turnoff();
             gpio_put(BLUE_LED_PIN, 1);
 
@@ -233,14 +229,14 @@ int main() {
                 show_action_message("Cartao SD", "Desmontado", "com Sucesso", 2500);
                 is_mount_runned = false;
                 needs_redraw = true;
-                counter = 0;
+                mount_counter = 0;
             } else {
                 leds_turnoff();
                 gpio_put(RED_LED_PIN, 1);
                 show_action_message("Falha ao", "Desmontar o", "Cartao SD", 2500);
                 is_mount_runned = true;
                 needs_redraw = true;
-                counter = 1;
+                mount_counter = 1;
             }
 
             leds_turnoff();
@@ -255,6 +251,7 @@ int main() {
             gpio_put(BLUE_LED_PIN, 1);
 
             if (file_open_counter == 0) {
+                // Abre o arquivo
                 res = f_open(&file, file_name, FA_WRITE | FA_CREATE_ALWAYS);
                 file_open_counter++;
             }
@@ -274,27 +271,36 @@ int main() {
             } else {
                 UINT bw;
                 needs_redraw = true;
+                char buffer_file[256];
 
+                // Escreve o cabeçalho do arquivo
                 if (file_counter == 0) {
-                    // buzzer_play(BUZZER_LEFT_PIN, 1000);
-                    // sleep_ms(250);
-                    // buzzer_stop(BUZZER_LEFT_PIN);
-                    // buzzer_play(BUZZER_LEFT_PIN, 1000);
-                    // sleep_ms(250);
-                    // buzzer_stop(BUZZER_LEFT_PIN);
+                    buzzer_play(BUZZER_LEFT_PIN, 1000);
+                    sleep_ms(250);
+                    buzzer_stop(BUZZER_LEFT_PIN);
+                    buzzer_play(BUZZER_LEFT_PIN, 1000);
+                    sleep_ms(250);
+                    buzzer_stop(BUZZER_LEFT_PIN);
 
-                    sprintf(buffer, "numero_amostra,accel_x,accel_y,accel_z,giro_x,giro_y,giro_z\n");
-                    res = f_write(&file, buffer, strlen(buffer), &bw);
-                    printf("cabecalho\n");
+                    sprintf(buffer_file, "tempo_ms,accel_x,accel_y,accel_z,giro_x,giro_y,giro_z\n");
+                    res = f_write(&file, buffer_file, strlen(buffer_file), &bw);
                 } else {
+                    absolute_time_t instant_time = to_ms_since_boot(get_absolute_time());
+
                     get_sensor_data();
 
-                    sprintf(buffer, "%d,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n",
-                        file_counter,
+                    if (file_counter == 1) {
+                        start_time = instant_time;
+                    }
+
+                    float elapsed_time = (instant_time - start_time) / 1000.0f;
+
+                    sprintf(buffer_file, "%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n",
+                        elapsed_time,
                         sensor_data.accel_x,sensor_data.accel_y,sensor_data.accel_z,
                         sensor_data.gyro_x,sensor_data.gyro_y,sensor_data.gyro_z
                     );
-                    res = f_write(&file, buffer, strlen(buffer), &bw);
+                    res = f_write(&file, buffer_file, strlen(buffer_file), &bw);
                 }
 
                 file_counter++;
@@ -309,13 +315,14 @@ int main() {
             gpio_put(BLUE_LED_PIN, 0);
             gpio_put(GREEN_LED_PIN, 1);
 
-            // buzzer_play(BUZZER_LEFT_PIN, 600);
-            // sleep_ms(250);
-            // buzzer_stop(BUZZER_LEFT_PIN);
-            // buzzer_play(BUZZER_LEFT_PIN, 600);
-            // sleep_ms(250);
-            // buzzer_stop(BUZZER_LEFT_PIN);
+            buzzer_play(BUZZER_LEFT_PIN, 600);
+            sleep_ms(250);
+            buzzer_stop(BUZZER_LEFT_PIN);
+            buzzer_play(BUZZER_LEFT_PIN, 600);
+            sleep_ms(250);
+            buzzer_stop(BUZZER_LEFT_PIN);
 
+            needs_redraw = true;
             show_action_message("Coleta de", "Dados", "Encerrada", 2500);
             // Reset de variáveis
             file_counter = 0;
@@ -334,7 +341,8 @@ int main() {
     return 0;
 }
 
-void show_action_message(const char* l1, const char* l2, const char* l3, uint32_t duration) {
+// Responsável por montar as menssagens temporárioas na tela
+static void show_action_message(const char* l1, const char* l2, const char* l3, uint32_t duration) {
     ssd1306_fill(&ssd, !color);
     ssd1306_send_data(&ssd);
 
@@ -346,7 +354,8 @@ void show_action_message(const char* l1, const char* l2, const char* l3, uint32_
     sleep_ms(duration);
 }
 
-void show_main_menu() {
+// Exibe a tela com o menu principal
+static void show_main_menu() {
     ssd1306_fill(&ssd, !color);
     ssd1306_send_data(&ssd);
 
@@ -354,12 +363,10 @@ void show_main_menu() {
         sprintf(buffer, "A - INICIAR");
     } else if (sampling_state == SAMPLING_RUNNING) {
         sprintf(buffer, "A - PARAR");
-    } else {
-        sprintf(buffer, "ENCERRANDO...");
     }
     ssd1306_draw_string(&ssd, buffer, 5, 20);
 
-    if (!is_mount_runned && counter == 0) {
+    if (!is_mount_runned && mount_counter == 0) {
         sprintf(buffer, "B - MONTAR");
     } else {
         sprintf(buffer, "B - DESMONTAR");
@@ -369,14 +376,16 @@ void show_main_menu() {
     ssd1306_send_data(&ssd);
 }
 
-void show_sampling_menu() {
+// Exibe a tela com o número de amostras coletadas em tempo real
+static void show_sampling_menu() {
     ssd1306_fill(&ssd, !color);
     ssd1306_send_data(&ssd);
 
     if (sampling_state == SAMPLING_RUNNING) {
+        sprintf(buffer, "Amostras Coletadas");
         sprintf(buffer, "QTND: %d", file_counter);
         ssd1306_draw_string(&ssd, buffer, 5, 30);
-        needs_redraw = true;
+        ssd1306_draw_string(&ssd, buffer, 5, 30);
     } else {
         sprintf(buffer, "Coleta");
         ssd1306_draw_string(&ssd, buffer, 5, 20);
@@ -384,18 +393,18 @@ void show_sampling_menu() {
         ssd1306_draw_string(&ssd, buffer, 5, 30);
         sprintf(buffer, "foi Iniciada");
         ssd1306_draw_string(&ssd, buffer, 5, 40);
-        needs_redraw = true;
     }
 
     ssd1306_send_data(&ssd);
 }
 
 // Função responsável por realizar o tratamento das interrupções geradas pelos botões
-void gpio_irq_handler(uint gpio, uint32_t events) {
+static void gpio_irq_handler(uint gpio, uint32_t events) {
     uint32_t current_time = to_ms_since_boot(get_absolute_time());
 
     if (gpio == BTN_A_PIN && (current_time - last_btn_a_press > DEBOUNCE_TIME)) { // inicia e para a amostragem do sensor MPU6050
         last_btn_a_press = current_time;
+        needs_redraw = true;
 
         if (sampling_state == SAMPLING_IDLE) {
             sampling_state = SAMPLING_RUNNING;
@@ -417,7 +426,7 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
 }
 
 // Captura os dados do sensor MPU6050
-void get_sensor_data() {
+static void get_sensor_data() {
     // Realiza a leitura dos sensores integrados no MPU6050
     mpu6050_read_raw(I2C0_PORT, accel, gyro, &temp);
 
@@ -434,4 +443,45 @@ void get_sensor_data() {
     printf("----\n");
     printf("ACCEL X: %.2f, Y: %.2f, Z: %.2f \n", sensor_data.accel_x, sensor_data.accel_y, sensor_data.accel_z);
     printf("GYRO X: %.2f, Y: %.2f, Z: %.2f \n", sensor_data.gyro_x, sensor_data.gyro_y, sensor_data.gyro_z);
+}
+
+static void process_stdio(int cRxedChar) {
+    static char cmd[256];
+    static size_t ix;
+
+    if (!isprint(cRxedChar) && !isspace(cRxedChar) && '\r' != cRxedChar &&
+        '\b' != cRxedChar && cRxedChar != (char)127)
+        return;
+    printf("%c", cRxedChar); // echo
+    stdio_flush();
+    if (cRxedChar == '\r') {
+        printf("%c", '\n');
+        stdio_flush();
+
+        if (!strnlen(cmd, sizeof cmd)) {
+            printf("> ");
+            stdio_flush();
+            return;
+        }
+        char *cmdn = strtok(cmd, " ");
+        if (cmdn) {
+           read_file(file_name);
+        }
+        ix = 0;
+        memset(cmd, 0, sizeof cmd);
+        printf("\n> ");
+        stdio_flush();
+    } else {
+        if (cRxedChar == '\b' || cRxedChar == (char)127) {
+            if (ix > 0) {
+                ix--;
+                cmd[ix] = '\0';
+            }
+        } else {
+            if (ix < sizeof cmd - 1) {
+                cmd[ix] = cRxedChar;
+                ix++;
+            }
+        }
+    }
 }
